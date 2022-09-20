e98f881c30d84f6fd410d5bf3a9ab0099605e379
Minjae <huna629@gmail.com>
2022-09-19 20:17:13 -0500

    Time: 30 ms (13.86%), Space: 46.5 MB (17.68%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()==0 && t.length()==0) return true;
         if(s.length()!=t.length()){ return false; }
+        /**
         HashMap<Character, Integer> maps = new HashMap<Character, Integer>();
         for(int i=0; i<s.length(); i++){
             if(maps.containsKey(s.charAt(i))){
@@ -19,5 +20,24 @@ class Solution {
             }
         }
         return true;
+        **/
+        HashMap<Character, Integer> mapS = new HashMap<Character, Integer>();
+        for(int i=0; i<s.length(); i++){
+            if(mapS.containsKey(s.charAt(i))){
+                mapS.put(s.charAt(i), mapS.get(s.charAt(i))+1);
+            }else{
+                mapS.put(s.charAt(i), 1);
+            }
+        }
+        
+        HashMap<Character, Integer> mapT = new HashMap<Character, Integer>();
+        for(int i=0; i<t.length(); i++){
+            if(mapT.containsKey(t.charAt(i))){
+                mapT.put(t.charAt(i), mapT.get(t.charAt(i))+1);
+            }else{
+                mapT.put(t.charAt(i), 1);
+            }
+        }
+        return mapS.equals(mapT);
     }
 }
