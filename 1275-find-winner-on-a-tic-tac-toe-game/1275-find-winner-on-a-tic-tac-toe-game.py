81fc5217f24a4f71e9d5d50718caf34ce105db56
Minjae <huna629@gmail.com>
2023-11-27 22:39:35 -0600

    Time: 47 ms (16.56%), Space: 16.3 MB (58.39%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,31 +1,19 @@
 from collections import defaultdict
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        move_X = [0]*9
-        move_O = [0]*9
-             
-        def check(arr):
-            return (arr[0]==arr[1]==arr[2]==1 or
-                    arr[3]==arr[4]==arr[5]==1 or
-                    arr[6]==arr[7]==arr[8]==1 or
-                    arr[0]==arr[3]==arr[6]==1 or
-                    arr[1]==arr[4]==arr[7]==1 or
-                    arr[2]==arr[5]==arr[8]==1 or
-                    arr[0]==arr[4]==arr[8]==1 or
-                    arr[2]==arr[4]==arr[6]==1)
-   
-        for i in range(0, len(moves)):
-            r, c = moves[i]
-            if i%2==0:
-                move_X[r*3+c]=1
-                if i>=4 and check(move_X):
-                    return "A"
-            else:
-                move_O[r*3+c]=1
-                if i>=4 and check(move_O): 
-                    return "B"
-            print(move_X, move_O)
+        A = [0]*8
+        B = [0]*8
         
-        if len(moves)<9: return "Pending"
-            
-        return "Draw"
+        for i in range(len(moves)):
+            r, c = moves[i]
+            player = A if i%2==0 else B
+            player[r]+=1
+            player[c+3]+=1
+            if r==c: player[6]+=1
+            if r==2-c: player[7]+=1
+        
+        for i in range(8):
+            if A[i]==3: return "A"
+            if B[i]==3: return "B"
+        
+        return "Draw" if len(moves)>=9 else "Pending"
